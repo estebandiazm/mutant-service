94ab03f88d0211b7cad95872da98656ca0b1d0ee
Juan Esteban Diaz <juesdimo@gmail.com>
2020-09-16 00:29:26 -0500

    Complete mutant validator algorithm and parametrized configuration

--- src/test/java/com/marvel/mutanservice/bussiness/MutantValidatorTest.java
+++ src/test/java/com/marvel/mutanservice/bussiness/MutantValidatorTest.java
@@ -2,20 +2,31 @@ package com.marvel.mutanservice.bussiness;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class MutantValidatorTest {
 
 
-    private MutantValidator mutantValidator = new MutantValidator();
+    @Autowired private MutantValidator mutantValidator;
 
     @Test
     void isMutant() {
-        List<String> adn = List.of("ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCCTA","TCACTG");
+        List<String> adn = List.of("ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG");
         boolean mutant = mutantValidator.isMutant(adn);
         Assertions.assertTrue(mutant);
+    }
+
+    @Test
+    void isNotMutant() {
+        List<String> adn = List.of("ATGCGA","CAGTGC","TTATTT","AGACGG","GCGTCA","TCACTG");
+        boolean mutant = mutantValidator.isMutant(adn);
+        Assertions.assertFalse(mutant);
     }
 }
