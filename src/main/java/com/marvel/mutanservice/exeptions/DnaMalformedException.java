a8b57b1732b1de9dc845d7a87f708c62ca2c5f53
Juan Esteban Diaz <juesdimo@gmail.com>
2020-09-18 12:03:28 -0500

    delete unnecessary fields

--- src/main/java/com/marvel/mutanservice/exeptions/DnaMalformedException.java
+++ src/main/java/com/marvel/mutanservice/exeptions/DnaMalformedException.java
@@ -5,14 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class DnaMalformedException extends RuntimeException{
-    public DnaMalformedException() {
-    }
-
     public DnaMalformedException(String message) {
         super(message);
     }
 
-    public DnaMalformedException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
